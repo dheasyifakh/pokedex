55ae23c38ed521a448bca20125b15b275aa7e87d
Dhea Syifa Khoirunnisa <dheasyifa.khoirunnisa@spentera.com>
2024-12-03 13:44:55 +0700

    Fix

--- components/pokemon-type/Sidebar.tsx
+++ components/pokemon-type/Sidebar.tsx
@@ -7,23 +7,6 @@ interface SidebarProps {
   }
 
 const Sidebar: React.FC<SidebarProps> = ({ setSelectedType, types, setCurrentPage}) => {
-//   const [types, setTypes] = useState<Type[]>([]);
-
-
-//   useEffect(() => {
-    
-//     const loadType = async () =>{
-//         try{
-//             const response = await fetch("https://pokeapi.co/api/v2/type");
-//             const data = await response.json();
-//             setTypes(data.results);
-//         }catch (error){
-            
-//         }
-//     }
-
-//     loadType();
-//   }, []);
 
   return (
     <List>
